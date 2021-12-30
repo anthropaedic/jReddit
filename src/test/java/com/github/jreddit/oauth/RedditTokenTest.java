3c65b44215992d14791d1d54d17afbb950e05acf
anthropaedic <hormel2332@gmail.com>
2021-12-30 01:14:31 -0600

    Open Pull Requests merged

--- src/test/java/com/github/jreddit/oauth/RedditTokenTest.java
+++ src/test/java/com/github/jreddit/oauth/RedditTokenTest.java
@@ -10,9 +10,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import com.github.jreddit.oauth.param.RedditScope;
+import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RedditTokenTest {
